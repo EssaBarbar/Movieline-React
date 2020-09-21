c8d20d314567caadf2a3002180263460cd34bd6d
Essa <essa992@gmail.com>
2020-09-21 08:58:40 +0200

    fixes

--- server/server.js
+++ server/server.js
@@ -2,22 +2,14 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config('.env')
 const fs = require('fs');
-const { json } = require('express');
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 app.use(cors())
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-
-
-app.get("/", (req, res) => { res.json("looool") })
-
-
 app.use(express.json())
-
 
 let pendingOrders = []
 
-
+app.get("/", (req, res) => { res.json("looool") })
 app.post("/create-checkout-session", async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
@@ -33,25 +25,20 @@ app.post("/create-checkout-session", async (req, res) => {
                 },
                 quantity: 1,
             }
+
         ],
         mode: "payment",
         success_url: "http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
         cancel_url: "http://localhost:3000/"
     });
-
     let pendingOrder = {
         orderId: session.id,
         order: req.body.cart,
     }
-
-
     pendingOrders.push(pendingOrder)
-
-
 
     res.json({ id: session.id });
 });
-
 
 app.post("/check-if-paid", async (req, res) => {
     const response = await stripe.checkout.sessions.retrieve(
@@ -86,4 +73,3 @@ app.post("/check-if-paid", async (req, res) => {
 });
 
 app.listen(4000, () => console.log('Server is running on port 4000'))
-
